63ad2e23d26a2b50dbb96219e5ef84060d7d5871
Anand <anand@Anands-MacBook-Pro.local>
2015-08-13 12:55:32 -0700

    first commit. added fn stubs.

---
+++ examples/stubs.hs
@@ -0,0 +1,32 @@
+data Message = Ping ProcessId
+             | Pong ProcessId
+  deriving (Typeable, Generic)
+
+
+instance Binary Message
+
+
+-- simple eg of receive a ping and then reply with a pong
+-- { v: Process () | \ex x : T . { stmt v = sseq ((srecv (Ping x)) (ssend x Message))}  where T = ?}
+f1 = do 
+       Ping from <- expect
+       mypid <- getSelfPid
+       send from (Pong mypid)
+
+
+-- { v: Process () | \ex x1 : T1, x2 : T2 . {stmt v = sseq ((srecv (Ping x1)) (
+--                                                      sseq (srecv (Ping x2)) 
+--														     (ssend x1 (Pong x2)) ))} where T1, T2 = ?}
+f2 = do
+	   Ping id1 <- expect
+	   Ping id2 <- expect
+	   send id1 (Pong id2)
+
+
+
+
+
+
+
+
+
