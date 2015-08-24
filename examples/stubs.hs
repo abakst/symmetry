6a0f894e46cf188c2564997da543b02269cb36fe
Anand <anand@Anands-MacBook-Pro.local>
2015-08-24 16:05:17 -0700

    Adding simple functions with their types.(anand)

--- examples/stubs.hs
+++ examples/stubs.hs
@@ -7,14 +7,14 @@ instance Binary Message
 
 
 -- simple eg of receive a ping and then reply with a pong
--- { v: Process () | \ex x : T . { stmt v = sseq ((srecv (Ping x)) (ssend x Message))}  where T = ?}
+-- \ex x : T .{ v: Process () |  { stmt v = sseq ((srecv (Ping x)) (ssend x Message))}  where T = ?}
 f1 = do 
        Ping from <- expect
        mypid <- getSelfPid
        send from (Pong mypid)
 
 
--- { v: Process () | \ex x1 : T1, x2 : T2 . {stmt v = sseq ((srecv (Ping x1)) (
+-- \ex x1 : T1, x2 : T2 .  { v: Process () |  {stmt v = sseq ((srecv (Ping x1)) (
 --                                                      sseq (srecv (Ping x2)) 
 --														     (ssend x1 (Pong x2)) ))} where T1, T2 = ?}
 f2 = do
