96f80fa8c48ebf23e0b965510a66d3298b017581
Rami Gökhan Kıcı <gokhankici@gmail.com>
2015-10-20 16:36:01 -0700

    stutter issue

--- checker/tests/pos/SrcStutter.hs
+++ checker/tests/pos/SrcStutter.hs
@@ -31,44 +31,32 @@ b_msg = inr tt
 
 stutter :: StutterSem repr => repr (Process ())
 stutter  = do role <- newRSing
-              p    <- spawn role (app my_sttr (lam $ \a -> app dosmt a))
+              p    <- spawn role (app sttr (lam $ \a -> app dosmt a))
               app sendAB p
               ret tt
 
+--if msg = 'a'
+--   then fail
+--   else ()
 dosmt :: StutterSem repr => repr (Msg -> Process ())
 dosmt  = lam $ \msg -> match msg fail (lam $ \_ -> ret tt)
 
+-- send the infinite sequence of ['a','b','a','b',...] messages
 sendAB :: StutterSem repr => repr ((Pid RSing) -> Process (Pid RSing))
 sendAB  = let fix_f = lam $ \f -> lam $ \pid -> do send pid a_msg
                                                    send pid b_msg
                                                    app f pid
            in lam $ \pid -> app (fixM fix_f) pid
 
--- sendA :: StutterSem repr => repr ((Pid RSing) -> Process ())
--- sendA  = lam $ \pid -> do send pid a_msg
---                           app sendB pid
-
--- sendB :: StutterSem repr => repr ((Pid RSing) -> Process ())
--- sendB = lam $ \pid -> do send pid b_msg
---                          app sendA pid
-
-my_sttr :: StutterSem repr => repr ((Msg->Process())->Process())
-my_sttr  = let fix_sttr = lam $ \sttr -> lam $ \f ->
+-- get 2 messages, call dosmt on the second one, repeat
+sttr :: StutterSem repr => repr ((Msg->Process())->Process())
+sttr  = let helper = lam $ \fix_sttr -> lam $ \f ->
                  do msg1 :: repr Msg <- recv
                     msg2 :: repr Msg <- recv
                     app f msg2
-                    app sttr f
-            in lam $ \f -> do app (fixM fix_sttr) f
-                              ret tt
-
--- sttr :: StutterSem repr => repr ((Msg->Process())->Process())
--- sttr  = lam $ \f -> do msg :: repr Msg <- recv
---                        app unsttr f
-
--- unsttr :: StutterSem repr => repr ((Msg->Process())->Process())
--- unsttr  = lam $ \f -> do msg :: repr Msg <- recv
---                          app f msg
---                          app sttr f
+                    app fix_sttr f
+         in lam $ \f -> do app (fixM helper) f
+                           ret tt
 
 
 main :: IO ()
