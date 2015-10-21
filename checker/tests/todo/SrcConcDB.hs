0c9efe8234f1401c8e31d0600ef8584ad5e3e53e
Rami Gökhan Kıcı <gokhankici@gmail.com>
2015-10-21 13:39:57 -0700

    converted sieve and concdb to fixpoint

--- checker/tests/todo/SrcConcDB.hs
+++ checker/tests/todo/SrcConcDB.hs
@@ -57,29 +57,36 @@ recv_value  = do msg :: repr ALV <- recv
                             match e1 fail id
 
 class ( Symantics repr
-      , SymSend   repr ALV
-      , SymRecv   repr ALV
-      , SymSend   repr LookupResT
-      , SymRecv   repr LookupResT
-      , SymSend   repr FreeAllocated
-      , SymRecv   repr FreeAllocated
-      , SymTypes  repr Int (Pid RSing)
-      , SymTypes  repr (Int, Pid RSing) (LookupT :+: ValueT)
-      , SymTypes  repr (Int, Pid RSing) ValueT
-      , SymTypes  repr () ()
-      , SymTypes  repr () Int
-      , SymTypes  repr Int Int
-      , SymMatch  repr () () Int
-      , SymMatch  repr () () (Process ())
-      , SymMatch  repr LookupT ValueT LookupT
-      , SymMatch  repr AllocT (LookupT :+: ValueT) AllocT
-      , SymMatch  repr () Int (Process ())
-      , SymMatch  repr () () (() :+: Int)
-      , SymMatch  repr AllocT (LookupT :+: ValueT) (Process ())
-      , SymMatch  repr (Int, Pid RSing) ValueT (Process ())
+      , SymSend repr ALV
+      , SymRecv repr ALV
+      , SymSend repr LookupResT
+      , SymRecv repr LookupResT
+      , SymSend repr FreeAllocated
+      , SymRecv repr FreeAllocated
+      , SymMatch repr () () (() :+: Int)
+      , SymMatch repr () () (Process ())
+      , SymMatch repr () () (Process (Process ()))
+      , SymMatch repr () () (Process T_db)
+      , SymMatch repr () () Int
+      , SymMatch repr () Int (Process ())
+      , SymMatch repr () Int (Process T_db)
+      , SymMatch repr (Int, Pid RSing) ValueT (Process ())
+      , SymMatch repr (Int, Pid RSing) ValueT (Process T_db)
+      , SymMatch repr AllocT (LookupT :+: ValueT) (Process ())
+      , SymMatch repr AllocT (LookupT :+: ValueT) (Process T_db)
+      , SymMatch repr AllocT (LookupT :+: ValueT) AllocT
+      , SymMatch repr LookupT ValueT LookupT
+      , SymTypes repr () ()
+      , SymTypes repr () Int
+      , SymTypes repr (Int, Pid RSing) (LookupT :+: ValueT)
+      , SymTypes repr (Int, Pid RSing) ValueT
+      , SymTypes repr Int (Pid RSing)
+      , SymTypes repr Int Int
+      , SymMatch repr () () (Process (Pid RSing))
+      , SymMatch repr () Int (Process (Pid RSing))
       ) => CDBSem repr
 
---instance CDBSem SymbEx
+instance CDBSem SymbEx
 
 concdb :: CDBSem repr => repr (Process ())
 concdb  = do r  <- newRSing
@@ -87,50 +94,67 @@ concdb  = do r  <- newRSing
              app spawnmany (app client db)
 
 spawnmany :: CDBSem repr => repr (Process () -> Process ())
-spawnmany  = lam $ \f -> do r <- newRSing
-                            spawn r f
-                            b <- app any_bool tt
-                            ifte b (app spawnmany f) (return tt)
+spawnmany  = lam $ \f ->
+               do let fix_sm = lam $ \spawnmany -> lam $ \f ->
+                                 do r <- newRSing
+                                    spawn r f
+                                    b <- app any_bool tt
+                                    ifte b (app spawnmany f) (ret f)
+                  app (fixM fix_sm) f
+                  ret tt
+
+type T_db = [(Int,Int)]
+
+f_database :: CDBSem repr
+           => repr ((T_db -> Process T_db) -> T_db -> Process T_db)
+f_database  = lam $ \database -> lam $ \l ->
+                do let allocHandler = lam $ \msg ->
+                         do let key = proj1 msg
+                                p   = proj2 msg
+                            match (app2 lookup key l)
+                              (lam $ \_ -> do send p free_msg
+                                              val <- recv_value
+                                              ifte (eq (proj2 val) p)
+                                                   (app database (cons (pair key (proj1 val)) l))
+                                                   fail)
+                              (lam $ \x -> do send p allocated_msg
+                                              app database l)
+                   let lookupHandler = lam $ \msg ->
+                         do let key = proj1 msg
+                                p   = proj2 msg
+                            send p (app2 lookup key l)
+                            app database l
+                   msg :: repr ALV <- recv
+                   match msg allocHandler $ lam $ \e1 ->
+                     match e1 lookupHandler fail
 
 database :: CDBSem repr => repr ([(Int,Int)] -> Process ())
 database  = lam $ \l ->
-              let allocHandler = lam $ \msg ->
-                    let key = proj1 msg
-                        p   = proj2 msg
-                     in match (app2 lookup key l)
-                          (lam $ \_ -> do send p free_msg
-                                          val <- recv_value
-                                          ifte (eq (proj2 val) p)
-                                               (app database (cons (pair key (proj1 val)) l))
-                                               fail)
-                          (lam $ \x -> do send p allocated_msg
-                                          app database l)
-                  lookupHandler = lam $ \msg ->
-                    let key = proj1 msg
-                        p   = proj2 msg
-                     in do send p (app2 lookup key l)
-                           app database l
-               in do msg :: repr ALV <- recv
-                     match msg allocHandler $ lam $ \e1 ->
-                       match e1 lookupHandler fail
+              do app (fixM f_database) l
+                 ret tt
+
+f_client :: CDBSem repr
+         => repr ((Pid RSing -> Process (Pid RSing)) -> Pid RSing -> Process (Pid RSing))
+f_client  = lam $ \client -> lam $ \db ->
+              do choice <- app any_bool tt
+                 me     <- self
+                 let insert_h = do k <- app any_nat tt
+                                   send db (app2 alloc_msg k me)
+                                   let free_h = lam $ \_ ->
+                                                  do v <- app any_nat tt
+                                                     send db (app2 value_msg v me)
+                                                     app client db
+                                       alloc_h = lam $ \_ -> app client db
+                                   msg :: repr FreeAllocated <- recv
+                                   match msg free_h alloc_h
+                     lookup_h = do k <- app any_nat tt
+                                   send db (app2 lookup_msg k me)
+                                   msg :: repr LookupResT <- recv
+                                   match msg
+                                     (lam $ \_ -> app client db)
+                                     (lam $ \x -> app client db)
+                 ifte choice insert_h lookup_h
 
 client :: CDBSem repr => repr (Pid RSing -> Process ())
-client  = lam $ \db ->
-            do choice <- app any_bool tt
-               me     <- self
-               let insert_h = do k <- app any_nat tt
-                                 send db (app2 alloc_msg k me)
-                                 let free_h = lam $ \_ ->
-                                       do v <- app any_nat tt
-                                          send db (app2 value_msg v me)
-                                          app client db
-                                     alloc_h = lam $ \_ -> app client db
-                                  in do msg :: repr FreeAllocated <- recv
-                                        match msg free_h alloc_h
-                   lookup_h = do k <- app any_nat tt
-                                 send db (app2 lookup_msg k me)
-                                 msg :: repr LookupResT <- recv
-                                 match msg
-                                   (lam $ \_ -> app client db)
-                                   (lam $ \x -> app client db)
-                in ifte choice insert_h lookup_h
+client  = lam $ \db -> do app (fixM f_client) db
+                          ret tt
--- checker/tests/todo/SrcSieve.hs
+++ checker/tests/todo/SrcSieve.hs
@@ -36,13 +36,19 @@ class ( Symantics repr
       , SymSend   repr Msg
       , SymMatch repr () () (() :+: ())
       , SymMatch repr () () (Process (Int -> Boolean))
+      , SymMatch repr () () (Process T_f3)
+      , SymMatch repr () (Pid RSing) (Process T_f3)
       , SymMatch repr (Pid RSing) Int (Pid RSing)
       , SymMatch repr (Pid RSing) Int Int
       , SymTypes repr () ()
+      , SymTypes repr () (Pid RSing)
+      , SymTypes repr (Int -> Boolean) (Pid RSing, T_ar3)
+      , SymTypes repr (Pid RSing) (Pid RSing)
       , SymTypes repr (Pid RSing) Int
+      , SymTypes repr (Pid RSing) T_ar3
       ) => SieveSem repr
 
---instance SieveSem SymbEx
+instance SieveSem SymbEx
 
 sieve_main :: SieveSem repr => repr (Process ())
 sieve_main  = do me    <- self
@@ -53,41 +59,63 @@ sieve_main  = do me    <- self
                  dump
 
 dump :: SieveSem repr => repr (Process ())
-dump  = do x :: repr Int <- recv
-           app print x
-           dump
+dump  = do let f_dump = lam $ \dump -> lam $ \_ ->
+                          do x :: repr Int <- recv
+                             app print x
+                             app dump tt
+           app (fixM f_dump) tt
 
 counter :: SieveSem repr => repr (Int -> Process ())
-counter  = lam $ \n -> do poke_from <- recv_poke
-                          send poke_from (app ans_msg n)
-                          app counter (plus n (repI 1))
+counter  = lam $ \n ->
+             do let f_counter = lam $ \counter -> lam $ \n ->
+                                  do poke_from <- recv_poke
+                                     send poke_from (app ans_msg n)
+                                     app counter (plus n (repI 1))
+                app (fixM f_counter) n
+                ret tt
 
 sieve :: SieveSem repr => repr (Pid RSing -> Pid RSing -> Process ())
 sieve  = lam $ \input -> lam $ \output ->
-           do me <- self
-              send input (app poke_msg me)
-              x <- recv_ans
-              send output x
-              r <- newRSing
-              f <- spawn r (app2 filter2 (app divisible_by x) input)
-              app2 sieve f output
+           do let f_sieve = lam $ \sieve -> lam $ \arg ->
+                              do let input  = proj1 arg
+                                     output = proj2 arg
+                                 me <- self
+                                 send input (app poke_msg me)
+                                 x <- recv_ans
+                                 send output x
+                                 r <- newRSing
+                                 f <- spawn r (app2 filter2 (app divisible_by x) input)
+                                 app sieve $ pair f output
+              app (fixM f_sieve) $ pair input output
+              ret tt
+
+type T_ar3 = () :+: Pid RSing
+type T_f3  = ((Int->Boolean),(Pid RSing,T_ar3))
+
+f_filter :: SieveSem repr
+         => repr ((T_f3 -> Process T_f3) -> T_f3 -> Process T_f3)
+f_filter  = lam $ \filter -> lam $ \arg ->
+              do let test     = proj1 arg
+                     input    = proj1 $ proj2 arg
+                     m_output = proj2 $ proj2 arg
+                 match m_output
+                   (lam $ \_ ->
+                      do from <- recv_poke -- filter2
+                         app filter $ pair3 test input (inr from))
+                   (lam $ \output ->
+                      do me <- self -- filter3
+                         send input (app poke_msg me)
+                         y <- recv_ans
+                         ifte (app test y)
+                           (app filter $ pair3 test input (inr output))
+                           (do send output (app ans_msg y)
+                               app filter $ pair3 test input (inl tt)))
 
 filter2 :: SieveSem repr
-        => repr ((Int->Boolean) -> Pid RSing -> Process (Int->Boolean))
-filter2  = lam $ \test -> lam $ \input -> do from <- recv_poke
-                                             app3 filter3 test input from
-
-filter3 :: SieveSem repr
-        => repr ((Int->Boolean) -> Pid RSing -> Pid RSing ->
-                 Process (Int->Boolean))
-filter3  = lam $ \test -> lam $ \input -> lam $ \output ->
-             do me <- self
-                send input (app poke_msg me)
-                y <- recv_ans
-                ifte (app test y)
-                  (app3 filter3 test input output)
-                  (do send output (app ans_msg y)
-                      app2 filter2 test input)
+        => repr ((Int->Boolean) -> Pid RSing -> Process ())
+filter2  = lam $ \test -> lam $ \input ->
+             do app (fixM f_filter) $ pair3 test input (inl tt)
+                ret tt
 
 divisible_by :: SieveSem repr => repr (Int -> Int -> Boolean)
 divisible_by = lam $ \x -> lam $ \y -> ifte (eq (app2 mod y x) (repI 0)) (inl tt) (inr tt)
