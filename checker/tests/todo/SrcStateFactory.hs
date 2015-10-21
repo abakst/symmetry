4848e987f2f3376a0416e8da5acdc391c89f2d23
Rami Gökhan Kıcı <gokhankici@gmail.com>
2015-10-21 11:30:35 -0700

    fixed some more fixpoint stuff

--- checker/tests/todo/SrcStateFactory.hs
+++ checker/tests/todo/SrcStateFactory.hs
@@ -22,9 +22,12 @@ class ( Symantics repr
       , SymRecv   repr Msg2
       , SymTypes repr (Pid RSing) Int
       , SymMatch repr () () (Process Int)
+      , SymTypes repr Int (Int -> Int -> Process Int)
+      , SymTypes repr Int (Int -> Process Int)
+      , SymMatch repr () () (Process (Int, Int -> Process Int))
       ) => SFSem repr
 
---instance SFSem SymbEx
+instance SFSem SymbEx
 
 create_msg1 :: SFSem repr => repr (Pid RSing -> Int -> Msg1)
 create_msg1  = lam $ \pid -> lam $ \n -> pair pid n
@@ -36,14 +39,24 @@ state_factory  = do n1 <- app any_nat tt
                     app2 call_loop n2 funWithState
                     ret tt
 
+type T_fs = (Int,(Int -> Int -> Process Int))
+
+f_state :: SFSem repr
+        => repr ((T_fs -> Process T_fs) -> T_fs -> Process T_fs)
+f_state  = lam $ \state -> lam $ \arg ->
+             do let n        = proj1 arg
+                let newstate = proj2 arg
+                msg1 :: repr Msg1 <- recv
+                let p     = proj1 msg1
+                    input = proj2 msg1
+                 in do m <- app2 newstate n input
+                       send p (m :: repr Msg2)
+                       app state $ pair m newstate
+
 state :: SFSem repr => repr (Int -> (Int -> Int -> Process Int) -> Process ())
 state  = lam $ \n -> lam $ \newstate ->
-           do msg1 :: repr Msg1 <- recv
-              let p     = proj1 msg1
-                  input = proj2 msg1
-               in do m <- app2 newstate n input
-                     send p (m :: repr Msg2)
-                     app2 state m newstate
+           do app (fixM f_state) (pair n newstate)
+              ret tt
 
 factory :: SFSem repr => repr (Int -> (Int -> Int -> Process Int) -> Process (Int -> Process Int))
 factory  = lam $ \n -> lam $ \newstate ->
@@ -55,12 +68,23 @@ factory  = lam $ \n -> lam $ \newstate ->
                           s :: repr Msg2 <- recv
                           ret s)
 
+type T_fcl = (Int,(Int -> Process Int))
+
+f_call_loop :: SFSem repr
+            => repr ((T_fcl -> Process T_fcl) -> T_fcl -> Process T_fcl)
+f_call_loop  =lam $ \call_loop -> lam $ \arg ->
+                do let n = proj1 arg
+                   let f = proj2 arg
+                   ifte (lt n (repI 1)) fail $
+                     ifte (eq n (repI 1))
+                       (do n <- app any_nat tt
+                           n' <- app f n
+                           ret $ pair n' f)
+                       (do n <- app any_nat tt
+                           app f n
+                           app call_loop $ pair (plus n (repI (-1))) f)
+
 call_loop :: SFSem repr => repr (Int -> (Int -> Process Int) -> Process Int)
 call_loop  = lam $ \n -> lam $ \f ->
-               ifte (lt n (repI 1)) fail $
-                 ifte (eq n (repI 1))
-                   (do n <- app any_nat tt
-                       app f n)
-                   (do n <- app any_nat tt
-                       app f n
-                       app2 call_loop (plus n (repI (-1))) f)
+               do res <- app (fixM f_call_loop) $ pair n f
+                  ret $ proj1 res
