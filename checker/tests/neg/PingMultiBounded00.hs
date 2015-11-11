0062421c4c7f6b42e4644aaf8a42f38eaf124ae9
Alexander Bakst <abakst@cs.ucsd.edu>
2015-11-11 14:40:20 -0800

    doN

---
+++ checker/tests/neg/PingMultiBounded00.hs
@@ -0,0 +1,36 @@
+{-# Language RebindableSyntax #-}
+{-# Language TypeOperators #-}
+{-# Language FlexibleContexts #-}
+{-# Language ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+module PingMulti00 where
+
+import Prelude hiding ((>>=), (>>), fail, return) 
+import SrcHelper
+import Symmetry.Language
+import Symmetry.Verify
+
+pingServer :: (DSL repr) => repr (Process repr ())
+pingServer = do myPid <- self
+                p     <- recv
+                send p myPid
+
+master :: forall repr. (DSL repr) => repr (RMulti -> Int -> Process repr ())
+master = lam $ \r -> lam $ \n ->
+   do ps <- spawnMany r n pingServer
+      myPid <- self
+      ifte arb
+        (doMany ps (lam $ \p -> send p myPid))
+        (return nil)
+      (doMany ps (lam $ \_ -> do (_ :: repr (Pid RSing))  <- recv
+                                 return tt))
+      return tt
+
+mainProc :: (DSL repr) => repr (Int -> ())
+mainProc = lam $ \n -> exec $ do r <- newRMulti
+                                 app (app master r) n
+
+main :: IO ()
+main = checkerMain (int 3 |> mainProc)
---
+++ checker/tests/pos/PingLoopBounded.hs
@@ -0,0 +1,30 @@
+{-# Language RebindableSyntax #-}
+{-# Language TypeOperators #-}
+{-# Language FlexibleContexts #-}
+{-# Language ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+module Main where
+
+import Prelude hiding ((>>=), (>>), fail, return) 
+import Symmetry.Language
+import Symmetry.Verify
+
+pingServer :: forall repr. (DSL repr) => repr (Int -> Process repr ())
+pingServer = lam $ \n ->
+               do doN n (lam $ \_ -> (recv :: repr (Process repr ())))
+                  return tt
+
+master :: (DSL repr) => repr (RSing -> Int -> Process repr ())
+master = lam $ \r -> lam $ \n ->
+   do p <- spawn r (app pingServer n)
+      doN n (lam $ \_ -> send p tt)
+      return tt
+
+mainProc :: (DSL repr) => repr (Int -> ())
+mainProc = lam $ \n -> exec $ do r <- newRSing
+                                 app (app master r) n
+
+main :: IO ()
+main = checkerMain (int 10 |> mainProc)
