0d0dc8e6028df66188f09084c92b1130cf98f81a
Rami Gökhan Kıcı <gokhankici@gmail.com>
2015-11-13 15:02:48 -0800

    added toy example for data abs.

---
+++ checker/tests/todo/DataAbs00.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TypeOperators #-}
+{-# Language RebindableSyntax #-}
+{-# Language ScopedTypeVariables #-}
+{-# Language FlexibleContexts #-}
+
+module Main where
+
+import Prelude hiding ((>>=), (>>), fail, return, id, lookup)
+import Symmetry.Language
+import Symmetry.Verify
+import Symmetry.SymbEx
+import SrcHelper
+
+sender :: DSL repr => repr (Pid RSing -> Process repr ())
+sender  = lam $ \pid -> send pid (int 0)
+
+receiver :: DSL repr => repr (Process repr ())
+receiver  = do msg :: repr Int <- recv
+               ifte (eq msg (int 0)) (return tt) (fail)
+
+master :: DSL repr => repr (Process repr ())
+master  = do r_rec <- newRSing
+             rec_pid <- spawn r_rec receiver
+             r_sender <- newRSing
+             spawn r_sender (app sender rec_pid)
+             ret tt
+
+main :: IO ()
+main  = checkerMain $ exec master
