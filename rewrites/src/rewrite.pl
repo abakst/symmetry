02b0133406e746907904b692bbeb7b2db54d1dc4
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2016-09-25 13:58:33 -0700

    While-loop/refactoring.
    -While-loop:
    	- rewrite to any smaller term (rather than skip only).
    	- try rewriting pairs in par([..]) before rewriting single expression.
    -Refactoring: removed rewrite of single expressions in par as it's
    subsumed by pair-wise rewrite.

--- rewrites/src/rewrite.pl
+++ rewrites/src/rewrite.pl
@@ -272,15 +272,9 @@ rewrite_step(T, Gamma, Delta, Rho, Psi, T1, Gamma1, Delta1, Rho1, Psi1) :-
 	      T1=skip, Gamma1=Gamma, Delta1=Delta, Rho1=Rho, Psi=Psi1
 	  ;   L = [A] ->
 	      T1=A, Gamma1=Gamma, Delta1=Delta, Rho1=Rho, Psi=Psi1
-	  /*
-	  rewrite single expression
-	  */
 	  ;   select(A, L, LR),
 	      (   A==skip->
 		  T1=par(LR), Gamma1=Gamma, Delta1=Delta, Rho1=Rho, Psi=Psi1
-	      ;   rewrite_step(A, Gamma, Delta, Rho, Psi, A1, Gamma1, Delta1, Rho1, Psi1)->
-		  select(A, L, A1, L1),
-		  T1=par(L1)
 	      )
 	  /*
 	  rewrite ordered pairs of expressions
@@ -449,10 +443,13 @@ rewrite_step(T, Gamma, Delta, Rho, Psi, T1, Gamma1, Delta1, Rho1, Psi1) :-
 	  TB=while(P, Cond, B),
 	  check_cond(Cond, P, Rho),
 	  empty_avl(Psi),
-          mk_pair(A, B, Pair),
-	  rewrite(Pair, Gamma, [], Rho, Psi, par(skip, skip), Gamma1, Delta2, Rho1, Psi1) ->
-	  T1=par(skip, TB),
-	  append(Delta, [Delta2], Delta1)
+          mk_pair(A, B, Pair, Switched),
+	  rewrite(Pair, Gamma, [], Rho, Psi, Pair1, Gamma, Delta2, Rho1, Psi1),
+          unswitch_pair(Pair1, Switched, par(A1, skip)),
+          smaller_than(A1, A) ->
+	  T1=par(A1, TB),
+	  append(Delta, [Delta2], Delta1),
+          Gamma1=Gamma
 	  /*
 	  par(A, B): rewrite ordered pairs.
 	  */
@@ -480,6 +477,14 @@ rewrite(T, Gamma, Delta, Rho, Psi, T2, Gamma2, Delta2, Rho2, Psi2) :-
 	;   format('Failed to rewrite term:~p~n' ,[T]), fail
 	).
 
+
+smaller_than(T, T1) :-
+	/* T is either a proper subterm of T1 or skip. */
+	(   T==skip
+	;   T1\==T,
+	    contains_term(T, T1)
+	).
+
 match(T, T1) :-
 	/*
 	Try to match T and T1 by permuting the elements of L in par(L).
@@ -494,9 +499,20 @@ match(T, T1) :-
 	).
 
 
+mk_pair(A, B, Pair, Switched) :-
+	(   Pair=par(A, B),
+	    Switched=false
+	;   Pair=par(B, A),
+	    Switched=true
+	).
+
 mk_pair(A, B, Pair) :-
-	(   Pair=par(A, B)
-	;   Pair=par(B, A)
+	mk_pair(A, B, Pair, _).
+
+unswitch_pair(par(A, B), Switched, Pair) :-
+	(   Switched->
+	    Pair=par(B, A)
+	;   Pair=par(A, B)
 	).
 
 sanity_check(L) :-
